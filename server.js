b51b04d89e7f41978e37702527d4e0e1d3afb6b3
SiddhantYerandkar <siddhantyerandkar@gmail.com>
2022-12-18 22:51:53 +0530

    chat app created

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const path = require('path')
 const http = require('http');
 const express = require('express')
 const socketio = require('socket.io')
-
+const formatMessage = require('./utils/messages')
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users')
 
 const app = express()
 const server = http.createServer(app)
@@ -12,9 +13,51 @@ const io = socketio(server)
 // set static Folder
 app.use(express.static(path.join(__dirname, 'public')))
 
+const botName = 'ChatCord Bot'
+
 // Run when client connects
-io.on('connection',function (socket) {
-    console.log("New WS Connection...");
+io.on('connection', socket => {
+    socket.on('joinRoom', ({ username, room }) => {
+        const user = userJoin(socket.id, username, room)
+
+        socket.join(user.room)
+
+
+        // Welcome current User
+        socket.emit('message', formatMessage(botName, 'Welcome to Chatcord!!'))
+
+        // BroadCast when a user connects
+        socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined the chat`))
+
+        // Send users and room info
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        })
+
+    })
+
+    // Listen for chatMessage
+    socket.on('chatMessage', msg => {
+        const user = getCurrentUser(socket.id)
+
+        io.to(user.room).emit('message', formatMessage(user.username, msg))
+    })
+
+    // Runs when client disconnects
+    socket.on('disconnect', () => {
+        const user = userLeave(socket.id)
+        if (user) {
+            io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`))
+
+            // Send users and room info
+            io.to(user.room).emit('roomUsers', {
+                room: user.room,
+                users: getRoomUsers(user.room)
+            })
+        }
+
+    })
 })
 
 
